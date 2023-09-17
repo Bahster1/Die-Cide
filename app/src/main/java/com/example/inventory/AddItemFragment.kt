f165bd2461918057862ae6237218732d681a73d3
Bahster <slybahwalsh27@gmail.com>
2023-09-17 15:37:33 -0700

    Finished Android Room tutorial. Link in InventoryApplication.kt

--- app/src/main/java/com/example/inventory/AddItemFragment.kt
+++ app/src/main/java/com/example/inventory/AddItemFragment.kt
@@ -22,15 +22,25 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.example.inventory.data.Item
 import com.example.inventory.databinding.FragmentAddItemBinding
 
 /**
  * Fragment to add or update an item in the Inventory database.
  */
-class AddItemFragment : Fragment() {
-
+class AddItemFragment : Fragment()
+{
     private val navigationArgs: ItemDetailFragmentArgs by navArgs()
+    private val viewModel: InventoryViewModel by activityViewModels {
+        InventoryViewModelFactory(
+            (activity?.application as InventoryApplication).database
+                .itemDao()
+        )
+    }
+    lateinit var item: Item
 
     // Binding object instance corresponding to the fragment_add_item.xml layout
     // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
@@ -45,6 +55,36 @@ class AddItemFragment : Fragment() {
     ): View? {
         _binding = FragmentAddItemBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.saveAction.setOnClickListener {
+            addNewItem()
+        }
+    }
+
+    private fun isEntryValid() : Boolean
+    {
+        return viewModel.isEntryValid(
+            binding.itemName.text.toString(),
+            binding.itemPrice.text.toString(),
+            binding.itemCount.text.toString()
+        )
+    }
+
+    private fun addNewItem()
+    {
+        if (isEntryValid())
+        {
+            viewModel.addNewItem(
+                binding.itemName.text.toString(),
+                binding.itemPrice.text.toString(),
+                binding.itemCount.text.toString()
+            )
+        }
+        val action = AddItemFragmentDirections.actionAddItemFragmentToItemListFragment()
+        findNavController().navigate(action)
     }
 
     /**
