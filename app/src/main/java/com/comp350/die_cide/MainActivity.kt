2c51b07ecad6348a63332e8d49c2038ca914a558
Ron Vincent Aspuria III <ronvincent.aspuria278@myci.csuci.edu>
2023-09-27 14:34:15 -0700

    First branch commit for Ron's Task2-3

--- app/src/main/java/com/comp350/die_cide/MainActivity.kt
+++ app/src/main/java/com/comp350/die_cide/MainActivity.kt
@@ -3,9 +3,11 @@ package com.comp350.die_cide
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
