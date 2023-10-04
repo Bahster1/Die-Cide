119c158a2781c4c15dcc5f39fd349f9919770dc6
Taylor Asplund <taylor.asplund@gmail.com>
2023-10-04 14:24:40 -0700

    we have liftoff (displaying a response from openai)

--- app/src/main/java/com/comp350/die_cide/MainActivity.kt
+++ app/src/main/java/com/comp350/die_cide/MainActivity.kt
@@ -2,12 +2,15 @@ package com.comp350.die_cide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var msgTV: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val response = Response().response("What is the date today?", 6)
-
+        msgTV = findViewById<TextView>(R.id.textView6)
+        msgTV.text = response
     }
 }
