2cc28537c83152f1dd87d43fcdec389b9cba156b
Morgan-AM <morgan.mcmurray076@myci.csuci.edu>
2023-10-04 13:43:46 -0700

    gui text input xml and beginnings of kotlin functionality
    QuestionInput.kt doesn't work

--- app/src/main/java/com/comp350/die_cide/MainActivity.kt
+++ app/src/main/java/com/comp350/die_cide/MainActivity.kt
@@ -2,10 +2,17 @@ package com.comp350.die_cide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.comp350.die_cide.QuestionInput.Companion.getUserInput
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding.submit.setOnClickListener {
+            var userQuestionInput = getUserInput(findViewById(com.google.android.material.R.id.search_view_edit_text))
+            Toast.makeText(applicationContext, "Your question: $userQuestionInput", Toast.LENGTH_SHORT).show()
+        }
     }
 }
