4f0d0f26be5a58aae205a29684e21931e2682047
Morgan-AM <morgan.mcmurray076@myci.csuci.edu>
2023-10-04 13:59:41 -0700

    MainActivity and QuestionInput works!
    The app doesn't crash when you enter in a question to the question field and submit with the submit button.  Toast outputs properly, all that good stuff

--- app/src/main/java/com/comp350/die_cide/MainActivity.kt
+++ app/src/main/java/com/comp350/die_cide/MainActivity.kt
@@ -2,6 +2,8 @@ package com.comp350.die_cide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.comp350.die_cide.QuestionInput.Companion.getUserInput
 
@@ -10,8 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        binding.submit.setOnClickListener {
-            var userQuestionInput = getUserInput(findViewById(com.google.android.material.R.id.search_view_edit_text))
+        val submitButton: Button = findViewById(R.id.submit)
+        val questionField: EditText = findViewById(R.id.userQuestion)
+
+        submitButton.setOnClickListener {
+            val userQuestionInput = getUserInput(questionField)
             Toast.makeText(applicationContext, "Your question: $userQuestionInput", Toast.LENGTH_SHORT).show()
         }
     }
