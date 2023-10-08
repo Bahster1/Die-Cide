c69ce086492f796a5da258321d5cc450356508de
Ron Vincent Aspuria III <ronvincent.aspuria278@myci.csuci.edu>
2023-10-07 22:57:37 -0700

    10/7/23 - For MainActivity.kt, added 'dieValue' to hold value of dice. (NOTE:This ONLY LIVES in the onCLickListener.) For DiceLogic.kt, added 'roll()' function. For AndroidManifest.xml, added lock to Portrait screen orientation.

--- app/src/main/java/com/comp350/die_cide/DiceLogic.kt
+++ app/src/main/java/com/comp350/die_cide/DiceLogic.kt
@@ -5,66 +5,64 @@ import android.os.CountDownTimer
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
-// this will be the main view model file that will effect the view which is (activity_main.xml)
-//TODO: FILE IS WORK IN PROGRESS
-//TODO: Return number correlating to dice face as a value
+// This is be the main view model file that will effect the view which is (activity_main.xml)
+
 
 class DiceLogic {
-    //fun clickDice(rollDiceMA : ImageView){}
+
     companion object : AppCompatActivity() {
-        /*
-        fun clickDice(rollDiceMA: ImageView) {
+        // LOGIC BLOCK
+        private var dieValue = 0
 
-            val diceMA = DiceRollLogic(20)
-            when (diceMA.rollDice()) {
-                1 -> rollDiceMA.setImageResource(R.drawable.dice_1)
-                2 -> rollDiceMA.setImageResource(R.drawable.dice_2)
-                3 -> rollDiceMA.setImageResource(R.drawable.dice_3)
-                4 -> rollDiceMA.setImageResource(R.drawable.dice_4)
-                5 -> rollDiceMA.setImageResource(R.drawable.dice_5)
-                6 -> rollDiceMA.setImageResource(R.drawable.dice_6)
-                7 -> rollDiceMA.setImageResource(R.drawable.dice_7)
-                8 -> rollDiceMA.setImageResource(R.drawable.dice_8)
-                9 -> rollDiceMA.setImageResource(R.drawable.dice_9)
-                10 -> rollDiceMA.setImageResource(R.drawable.dice_10)
-                11 -> rollDiceMA.setImageResource(R.drawable.dice_11)
-                12 -> rollDiceMA.setImageResource(R.drawable.dice_12)
-                13 -> rollDiceMA.setImageResource(R.drawable.dice_13)
-                14 -> rollDiceMA.setImageResource(R.drawable.dice_14)
-                15 -> rollDiceMA.setImageResource(R.drawable.dice_15)
-                16 -> rollDiceMA.setImageResource(R.drawable.dice_16)
-                17 -> rollDiceMA.setImageResource(R.drawable.dice_17)
-                18 -> rollDiceMA.setImageResource(R.drawable.dice_18)
-                19 -> rollDiceMA.setImageResource(R.drawable.dice_19)
-                20 -> rollDiceMA.setImageResource(R.drawable.dice_20)
-            }
-
-
+        fun roll(): Int {
+            dieValue = (1..20).random()
+            return dieValue
         }
-        */
 
-        private var diceAnimation: AnimationDrawable? = null
+        // ANIMATION BLOCK
+        var diceAnimation: AnimationDrawable? = null
         private var setTime: CountDownTimer? = null
-        fun onPlay(diceImage : ImageView) {
+        fun onPlay(diceImage : ImageView , dieValue : Int){
             val randomTime = (1000..3000).random().toLong()
             setTime = object : CountDownTimer(randomTime, 1000) {
+                // For every Tick, a random dice image will be selected and replace the dice image
                 override fun onTick(p0: Long) {
                     diceImage.setImageResource(androidx.appcompat.R.drawable.abc_control_background_material)
                     diceImage.setBackgroundResource(R.drawable.animate_dice)
                     diceAnimation = diceImage.background as AnimationDrawable
                     diceAnimation?.start()
                 }
-
+                // Once finished, the animation will end on a dice face based on 'dieValue'
                 override fun onFinish() {
                     diceAnimation?.stop()
-                    //Toast.makeText(this,"DiceRolled!",Toast.LENGTH_SHORT).show()
+                    when (dieValue) {
+                        1 -> diceImage.setImageResource(R.drawable.dice_1)
+                        2 -> diceImage.setImageResource(R.drawable.dice_2)
+                        3 -> diceImage.setImageResource(R.drawable.dice_3)
+                        4 -> diceImage.setImageResource(R.drawable.dice_4)
+                        5 -> diceImage.setImageResource(R.drawable.dice_5)
+                        6 -> diceImage.setImageResource(R.drawable.dice_6)
+                        7 -> diceImage.setImageResource(R.drawable.dice_7)
+                        8 -> diceImage.setImageResource(R.drawable.dice_8)
+                        9 -> diceImage.setImageResource(R.drawable.dice_9)
+                        10 -> diceImage.setImageResource(R.drawable.dice_10)
+                        11 -> diceImage.setImageResource(R.drawable.dice_11)
+                        12 -> diceImage.setImageResource(R.drawable.dice_12)
+                        13 -> diceImage.setImageResource(R.drawable.dice_13)
+                        14 -> diceImage.setImageResource(R.drawable.dice_14)
+                        15 -> diceImage.setImageResource(R.drawable.dice_15)
+                        16 -> diceImage.setImageResource(R.drawable.dice_16)
+                        17 -> diceImage.setImageResource(R.drawable.dice_17)
+                        18 -> diceImage.setImageResource(R.drawable.dice_18)
+                        19 -> diceImage.setImageResource(R.drawable.dice_19)
+                        20 -> diceImage.setImageResource(R.drawable.dice_20)
+                    }
                 }
 
             }.start()
 
-
         }
-
+        // Function to stop 'setTime' Timer to prevent infinite counter
         override fun onDestroy() {
             super.onDestroy()
             if (setTime != null) {
@@ -75,11 +73,5 @@ class DiceLogic {
 
     }
 }
-/*
-class DiceRollLogic(private val diceMaxSides: Int) {
-    fun rollDice(): Int {
-        return (1..diceMaxSides).random()
-    }
-}
-*/
+
 
--- app/src/main/java/com/comp350/die_cide/MainActivity.kt
+++ app/src/main/java/com/comp350/die_cide/MainActivity.kt
@@ -2,7 +2,9 @@ package com.comp350.die_cide
 
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+
 
 //TODO: REMEMBER TO OPTIMIZE IMPORTS BEFORE FINAL COMMIT AND PUSH
 class MainActivity : AppCompatActivity() {
@@ -11,14 +13,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollDiceMA: ImageView = findViewById(R.id.diceBtn)
-        //rollDiceMA.setOnClickListener {
-        //    DiceLogic.clickDice(rollDiceMA)
-        //}
+        // initialize variables for dice image and value
+        val diceImage: ImageView = findViewById(R.id.diceBtn)
+        var dieValue : Int
 
-        rollDiceMA.setOnClickListener {
-            DiceLogic.onPlay(rollDiceMA)   //CALL ANIMATION BLOCK
+        // Actions to occur once dice is clicked on
+        diceImage.setOnClickListener {
+            dieValue = DiceLogic.roll()             // DICE LOGIC BLOCK
+            DiceLogic.onPlay(diceImage, dieValue)   // DICE ANIMATION BLOCK
 
+            //testing to ensure "dieValue" is passed correctly - lives only in the Click event
+            val diceTest : TextView = findViewById(R.id.textView4)
+            diceTest.text = dieValue.toString()
         }
 
 
