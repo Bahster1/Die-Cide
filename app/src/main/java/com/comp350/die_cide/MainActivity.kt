95553b66b70eb62a544a82f9cb339b5248881218
Morgan-AM <morgan.mcmurray076@myci.csuci.edu>
2023-10-16 11:43:53 -0700

    Added comments and clarified names
    Inserted comments to describe what blocks of code in MainActivity.kt and QuestionInput.kt do, and changed some variable names to better describe what each variable and function is for

--- app/src/main/java/com/comp350/die_cide/MainActivity.kt
+++ app/src/main/java/com/comp350/die_cide/MainActivity.kt
@@ -11,7 +11,7 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.comp350.die_cide.QuestionInput.Companion.getUserInput
+import com.comp350.die_cide.QuestionInput.Companion.getUserQuestion
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -21,6 +21,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var openAIResponse: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // initialize variables for viewing the main app screen
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         openAIResponse = findViewById(R.id.textView6)
@@ -29,17 +31,19 @@ class MainActivity : AppCompatActivity() {
         val diceImage: ImageView = findViewById(R.id.diceBtn)
         var dieValue : Int
         val questionField: EditText = findViewById(R.id.userQuestion)
-        var userQuestionInput: String
+        var userQuestion: String
         var response: String?
 
         // Actions to occur once dice is clicked on
         diceImage.setOnClickListener {
             dieValue = DiceLogic.roll()   // DICE LOGIC BLOCK
             DiceLogic.onPlay(diceImage)   // DICE ANIMATION BLOCK
-            userQuestionInput = getUserInput(questionField)
+            userQuestion = getUserQuestion(questionField)
+
+            // Enables dice animation to run throughout the duration of obtaining an OpenAI response
             CoroutineScope(Dispatchers.Main).launch {
                 response = withContext(Dispatchers.IO) {
-                    Response().response(userQuestionInput, dieValue)
+                    Response().response(userQuestion, dieValue)
                 }
                 DiceLogic.displayDiceFace(diceImage, dieValue)
                 openAIResponse.text = response
