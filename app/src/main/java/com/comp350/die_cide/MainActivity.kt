9663f00e78507cffb3da06334a0033436c96dbd3
Ron Vincent Aspuria III <ronvincent.aspuria278@myci.csuci.edu>
2023-11-15 19:17:19 -0800

    11/15/23 - Added Compose elements for all UI items in MainActivity.kt. Now we need to put back in the logic.

--- app/src/main/java/com/comp350/die_cide/MainActivity.kt
+++ app/src/main/java/com/comp350/die_cide/MainActivity.kt
@@ -9,36 +9,29 @@
 */
 package com.comp350.die_cide
 
-import android.app.Activity
-import android.content.ActivityNotFoundException
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.speech.RecognizerIntent
-import android.view.Menu
-import android.view.MenuItem
-import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.comp350.die_cide.QuestionInput.Companion.getUserQuestion
-import com.comp350.die_cide.data.Interaction
-import com.comp350.die_cide.data.InteractionDao
-import com.comp350.die_cide.data.InteractionRoomDatabase
+import androidx.compose.ui.unit.dp
 import com.comp350.die_cide.ui.theme.DieCideTheme
-import com.google.android.material.snackbar.Snackbar
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+
 
 // TODO LIST:
 // [DONE]  Make sure all necessary compose libraries are in the gradle files
@@ -49,12 +42,12 @@ import kotlinx.coroutines.withContext
 // Delete the old XML-based code once it is clear that the Jetpack Compose code works
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var openAIResponseDisplay: TextView
-    private lateinit var db: InteractionRoomDatabase
-    private lateinit var interactionDao: InteractionDao
-    private lateinit var interaction: Interaction
-    private lateinit var questionField: EditText
+    // TODO: Adapt these left over variables (if applicable)
+//    private lateinit var openAIResponseDisplay: TextView
+//    private lateinit var db: InteractionRoomDatabase
+//    private lateinit var interactionDao: InteractionDao
+//    private lateinit var interaction: Interaction
+//    private lateinit var questionField: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
 
 
@@ -66,121 +59,10 @@ class MainActivity : AppCompatActivity() {
                 MainScreenPreview()
             }
         }
-        openAIResponseDisplay = findViewById(R.id.OpenAIResponse)
-        db = InteractionRoomDatabase.getDatabase(this)
-        interactionDao = db.interactionDao()
-
-
-        val diceImage: ImageView = findViewById(R.id.diceBtn)
-        var diceValue: Int
-        questionField = findViewById(R.id.userQuestion)
-        var userQuestion: String
-        var openAIResponse: String?
-        val micBtn: ImageView = findViewById(R.id.micImage)
-
-
-        micBtn.setOnClickListener {
-            questionField.text = null
-            startSpeechToText()
-        }
-
-
-        diceImage.setOnClickListener {
-            userQuestion = getUserQuestion(questionField)
-
-
-            if (userQuestion.isBlank()) {
-                Snackbar.make(
-                    findViewById(R.id.MiddleConstraintLayout),
-                    "Please enter a question",
-                    Snackbar.LENGTH_SHORT
-                ).show()
-
-            } else {
-                hideKeyboard()
-//                val mediaPlayer = MediaPlayer.create(this,R.raw.rolling_dice_sfx)
-//                mediaPlayer.start()
-                diceValue = DiceLogic.roll()   // DICE LOGIC BLOCK
-                DiceLogic.playDiceAnimation(diceImage, 5000)   // DICE ANIMATION BLOCK
-
-
-                // Enables dice animation to run throughout the duration of obtaining an OpenAI response
-                CoroutineScope(Dispatchers.Main).launch {
-                    openAIResponse = withContext(Dispatchers.IO) {
-                        Response().getResponse(userQuestion, diceValue)
-                    }
-
-                    DiceLogic.displayDiceFace(diceImage, diceValue)
-                    openAIResponseDisplay.text = openAIResponse
-
-
-                    interaction = Interaction(
-                        question = userQuestion,
-                        number = diceValue,
-                        answer = openAIResponse
-                    )
-                    interactionDao.insert((interaction))
-                }
-
-//                mediaPlayer.release()
-            }
-        }
     }
 
-    // SPEECH TO TEXT BLOCK
-    // 10/30/23 - This will be implemented in QuestionInput.kt later and cleaned up. For the
-    // sprint demo, I assumed working code is better than clean code FOR NOW.
-    // THIS WAS CHOSEN FOR CLASS COHESION - PUT THIS IN QuestionInput.kt or in a new class
-    private fun startSpeechToText() {
-        val speechIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-        speechIntent.putExtra(
-            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
-        )
-        speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-US")
 
-        try {
-            startActivityForResult(speechIntent, 1)
-        } catch (e: ActivityNotFoundException) {
-            Toast.makeText(
-                this,
-                "Speech recognition not available on this device",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-    }
 
-    //Confirms use of startSpeechToText function
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
-            val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
-            if (!result.isNullOrEmpty()) {
-                val spokenText = result[0]
-                questionField.setText(spokenText)
-
-            }
-
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.history -> startActivity(Intent(this, HistoryActivity::class.java))
-        }
-
-        return super.onOptionsItemSelected(item)
-    }
-
-    private fun hideKeyboard() {
-        val keyboardHider = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        keyboardHider.hideSoftInputFromWindow(questionField.windowToken, 0)
-    }
 
     @Preview(showBackground = true)
     @Composable
@@ -188,19 +70,76 @@ class MainActivity : AppCompatActivity() {
         MainScreen()
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun MainScreen() {
-        // TODO: THIS WILL BE WHERE ALL UI ELEMENTS WILL GO
+        // TODO: BRING BACK ALL NECESSARY VARIABLES
+        //Question prompt variables
+        var questionField by remember { mutableStateOf("") }
+        //Open AI variables
+        var openAIResponseDisplay by remember { mutableStateOf("") }
+        //Dice logic variables
+        var diceValue : Int
+        //Speech-to-text variables
+        //Rooms Database variables
+
+
         Row {
             // Title and Menu button
+
         }
         Row {
-            Column {
+            Column (modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
                 // Prompt field
+                TextField(
+                    value = questionField,
+                    onValueChange = { questionField = it },
+                    label = { Text("Type your question here:") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+
+                )
+                // Mic Button for Speech to text
+                Image(
+                    painter = painterResource(id = R.drawable.mic_image),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(50.dp)
+                        .padding(16.dp)
+                        .clickable {
+                            // Speech-to-text logic
+                        }
+                )
+
                 // Dice image
+                Image(
+                    painter = painterResource(id = R.drawable.dice_20),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(200.dp)
+                        .padding(16.dp)
+                        .clickable {
+                            // Dice Logic
+                        }
+                )
+
                 // Response field
+                Text(
+                    text = openAIResponseDisplay,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                )
+            }
+
+
             }
         }
-    }
 
 }
